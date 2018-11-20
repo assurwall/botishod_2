e6cc6eb05309778732dd4f19029b9827756df55e
SashaKrotov <zaooozi57@gmail.com>
2018-11-20 18:22:24 +0300

    auto commit

--- data.py
+++ data.py
@@ -132,13 +132,11 @@ def update_db(users_name):
     
     cur.execute('CREATE TABLE users_data (chat_id int NOT NULL UNIQUE, first_name varchar(30) NOT NULL, user_name varchar(30))')
     
-    for chat_id, name in users_name.items():
+    for chat_id in users_name.keys():
         
-        first_name = str(name[0])
+        first_name = str(users_name.get(chat_id)[0])
 
-        user_name = str(name[1])
-        
-        print('Value= '+str(chat_id)+' '+first_name+' '+user_name+'\n')
+        user_name = str(users_name.get(chat_id)[1])
         
         cur.execute("INSERT INTO users_data VALUES ("+str(chat_id)+",'"+first_name+"','"+user_name+"')")
         
