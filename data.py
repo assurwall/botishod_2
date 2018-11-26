01face1839345c6fca0041cd2ecec613f7248c93
SashaKrotov <zaooozi57@gmail.com>
2018-11-26 13:35:43 +0300

    auto commit

--- data.py
+++ data.py
@@ -288,12 +288,10 @@ def increment_buttons_db(button_id):
     
     if(value == None):
         
-        print('Значение по текущей дате None')
+        print('Value не None, Value='+value)
         
         value = [0, 0, 0, 0, 0, 0]
-    
-    print('Value = '+value)
-    
+
     value[button_id] += 1
 
     cur.execute("INSERT INTO statistics_buttons (date, hl, inf, cn, ln, lg) VALUES ('"+str(datetime.date.today())+"',"+str(value[1])+","+str(value[2])+","+str(value[3])+","+str(value[4])+","+str(value[5])+") ON CONFLICT (date) DO UPDATE SET hl="+str(value[1])+", inf="+str(value[2])+", cn="+str(value[3])+", ln="+(value[4])+",lg="+str(value[5]))
