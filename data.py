37a428ad0b5b887079e9db36a58e03db96d211bc
SashaKrotov <zaooozi57@gmail.com>
2018-11-26 15:06:26 +0300

    auto commit

--- data.py
+++ data.py
@@ -312,7 +312,7 @@ def increment_buttons_db(button_id):
     
     print('Array = '+str(array))
     
-    set_value_str="hl="+str(array[1])+", inf="+str(array[2])+", cn="+str(array[3])+", ln="+(array[4])+",lg="+str(array[5])
+    set_value_str="hl="+str(array[1])+", inf="+str(array[2])+", cn="+str(array[3])+", ln="+str(array[4])+",lg="+str(array[5])
     
     print('5.5')
 
