35204b3162d18ae89fc1a3e7920321fdd5b25949
Admin <krototip@gmail.com>
2019-01-14 14:54:34 +0300

    auto commit

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 # Бот с именем @PomothBot
 
-#token = '531376255:AAGkYzkzW7qRzY_vzvhKRCjzB3sBlMSGH6I'
+token = '531376255:AAGkYzkzW7qRzY_vzvhKRCjzB3sBlMSGH6I'
 
 # Бот с именeм @test1819_bot
 
@@ -10,4 +10,4 @@
 
 # Бот с именем @ishod_test_bot.
 
-token = '676055504:AAEO_XW1g9J5bbNGhnN35xrOdTYB6NCZoOI'
+#token = '676055504:AAEO_XW1g9J5bbNGhnN35xrOdTYB6NCZoOI'
